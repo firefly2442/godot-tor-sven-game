001039bcaa3d62c9ca9be9e3ae839767d7552683
firefly2442 <firefly2442@gmail.com>
2025-07-27 22:43:55 -0500

    feat: start adding some player vehicles as resources

---
+++ scenes/entities/player_vehicles/vehicle_resource.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name vehicle_resource
+
+@export var speed: float ## the vehicle speed
+@export var vehicletype: vehicle_type ## the type of vehicle
+
+enum vehicle_type {
+	AMBULANCE,
+	FIRETRUCK,
+	POLICECAR,
+	SCHOOLBUS,
+	TOWTRUCK
+}
