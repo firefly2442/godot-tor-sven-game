8581a7cd301cb651ca6a58925b5b40c3c053aee7
firefly2442 <firefly2442@gmail.com>
2025-08-01 18:27:06 -0500

    feat: work on implementing the water hose particles

--- scripts/entities/WaterHose.gd
+++ scripts/entities/WaterHose.gd
@@ -1,36 +1,11 @@
 extends Node2D
 
-@export var max_emission_rate: float = 400.0
-@export var min_emission_rate: float = 50.0
-@export var pressure: float = 1.0 # 0..1
-@export var splash_scene: PackedScene  # assign a splash particle scene
-@export var impact_distance: float = 800.0
-
 
 func _ready() -> void:
-	$RayCast2D.target_position = Vector2(impact_distance, 0)
-	update_emission()
+	$GPUParticles2D.emitting = false
 
-func set_pressure(p: float) -> void:
-	pressure = clamp(p, 0.0, 1.0)
-	update_emission()
-
-func update_emission() -> void:
-	# scale emission rate and particle speed with pressure
-	#var material: ParticleProcessMaterial = stream.process_material
-	#stream.emission_rate = lerp(min_emission_rate, max_emission_rate, pressure)
-	#material.initial_velocity = lerp(200, 800, pressure)
-	# optionally scale size/intensity
-	#stream.scale = lerp(0.6, 1.2, pressure)
-	pass
 
 func _physics_process(_delta: float) -> void:
-	# point the ray in hose direction
-	#ray.cast_to = Vector2(impact_distance, 0).rotated(global_rotation)
-	#ray.force_raycast_update()
-	#if ray.is_colliding():
-		#var hit_pos = ray.get_collision_point()
-		#spawn_splash(hit_pos)
 	pass
 
 func _process(_delta: float) -> void:
@@ -43,3 +18,11 @@ func _process(_delta: float) -> void:
 	if hose_direction.length_squared() > 0.0001:
 		# Set rotation so local +X points toward input direction.
 		rotation = hose_direction.angle()
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Utility.player1_selected == "Operator" and Input.is_action_just_pressed("action_p1") or \
+		Utility.player2_selected == "Operator" and Input.is_action_just_pressed("action_p2"):
+		if $GPUParticles2D.emitting:
+			$GPUParticles2D.emitting = false
+		else:
+			$GPUParticles2D.emitting = true
