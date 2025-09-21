c90b5b00828927a87c475b7154d538ad1eb29ad4
firefly2442 <firefly2442@gmail.com>
2025-09-21 11:25:18 -0500

    feat: finish options page

--- scripts/Options.gd
+++ scripts/Options.gd
@@ -1,44 +1,64 @@
 extends Node2D
 
-@onready var _settings: Dictionary = {resolution = Vector2i(1920, 1080), fullscreen = false, vsync = false}
+var _settings: Dictionary = {fullscreen = false}
+@onready var selectable_items: Array = [%Fullscreen, %BackgroundMusicHSlider, %SoundEffectsHSlider, %BackButton]
+var current_selected_index: int = 0
+var move_cooldown: float = 0.3  # seconds between menu moves
+var time_until_next: float = 0.0
 
 func _ready() -> void:
+	self.time_until_next = 0.5
+	
 	self._settings.fullscreen = ((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))
 	%Fullscreen.button_pressed = self._settings.fullscreen
 	
-	self._settings.vsync = (DisplayServer.window_get_vsync_mode() != DisplayServer.VSYNC_DISABLED)
-	%VSync.button_pressed = self._settings.vsync
-	
-	for res: int in range(0, %Resolution.get_item_count()):
-		var res_str: String = %Resolution.get_item_text(res)
-		var values: Array = res_str.split("x")
-		if get_window().get_size() == Vector2i(int(values[0]), int(values[1])):
-			%Resolution.selected = res
-	
 	%BackgroundMusicHSlider.value = AudioManager.backgroundvolume
 	%SoundEffectsHSlider.value = AudioManager.effectsvolume
-
-
-func _on_save_btn_pressed() -> void:
-	var res_str: String = %Resolution.get_item_text(%Resolution.get_selected_id())
-	var values: Array = res_str.split("x")
-	self._settings.resolution = Vector2i(int(values[0]), int(values[1]))
 	
+	%Fullscreen.grab_focus()
+
+
+func _process(delta: float) -> void:
+	if time_until_next > 0.0:
+		time_until_next -= delta
+		
+	if time_until_next <= 0.0:
+		if Input.is_action_just_pressed("back_p1") or Input.is_action_just_pressed("back_p2") or \
+		Input.is_action_just_pressed("escape_p1") or Input.is_action_just_pressed("escape_p2"):
+			SceneSwitcher.switch_scene(UID.CORE.MAINMENU)
+			AudioManager.playUIClick()
+		
+		if Input.is_action_pressed("up_p1") or Input.is_action_pressed("up_p2"):
+			current_selected_index -= 1
+			current_selected_index = clamp(current_selected_index, 0, selectable_items.size()-1)
+			
+			AudioManager.playUIClick()
+			selectable_items[current_selected_index].grab_focus()
+			time_until_next = move_cooldown
+		elif Input.is_action_pressed("down_p1") or Input.is_action_pressed("down_p2"):
+			current_selected_index += 1
+			current_selected_index = clamp(current_selected_index, 0, selectable_items.size()-1)
+			
+			AudioManager.playUIClick()
+			selectable_items[current_selected_index].grab_focus()
+			time_until_next = move_cooldown
+
+func _on_fullscreen_pressed() -> void:
 	self._settings.fullscreen = %Fullscreen.button_pressed
-	self._settings.vsync = %VSync.button_pressed
-	
-	AudioManager.backgroundvolume = %BackgroundMusicHSlider.value
-	AudioManager.effectsvolume = %SoundEffectsHSlider.value
 	
 	# apply the settings
 	# https://www.gdquest.com/tutorial/godot/2d/settings-demo/
 	get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (self._settings.fullscreen) else Window.MODE_WINDOWED
-	get_window().set_size(self._settings.resolution)
-	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED if (self._settings.vsync) else DisplayServer.VSYNC_DISABLED)
 
 
-func _unhandled_input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("back_p1") or Input.is_action_just_pressed("back_p2") or \
-	Input.is_action_just_pressed("escape_p1") or Input.is_action_just_pressed("escape_p2"):
-		SceneSwitcher.switch_scene(UID.CORE.MAINMENU)
-		AudioManager.playUIClick()
+func _on_back_button_pressed() -> void:
+	SceneSwitcher.switch_scene(UID.CORE.MAINMENU)
+
+
+func _on_background_music_h_slider_value_changed(_value: float) -> void:
+	AudioManager.backgroundvolume = %BackgroundMusicHSlider.value
+
+
+func _on_sound_effects_h_slider_value_changed(_value: float) -> void:
+	AudioManager.effectsvolume = %SoundEffectsHSlider.value
+	AudioManager.playUIClick()
